97da80ff2fa1a81dfe19766ad51da9c04e1c4746
steamedEggMaster <cksdud5014@naver.com>
2024-04-28 13:16:23 +0900

    [SecondChance] StartProject

---
+++ src/main/java/com/example/secondchanceback/SecondChanceBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.secondchanceback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecondChanceBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecondChanceBackApplication.class, args);
+    }
+
+}
