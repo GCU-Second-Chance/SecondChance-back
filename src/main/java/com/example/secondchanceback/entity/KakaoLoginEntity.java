e3bebccd44cc532f069b7f95d4c4ee986351972b
steamedEggMaster <cksdud5014@naver.com>
2024-04-30 22:33:42 +0900

    [develop] start project

---
+++ src/main/java/com/example/secondchanceback/entity/KakaoLoginEntity.java
@@ -0,0 +1,13 @@
+package com.example.secondchanceback.entity;
+
+/**
+ * @PackageName : com.example.secondchanceback.entity
+ * @FileName : KakaoLoginEntity
+ * @Author : noglass_gongdae
+ * @Date : 2024-04-30
+ * @Blog : https://blog.naver.com/noglass_gongdae
+ * @GitHub :
+ */
+public class KakaoLoginEntity {
+
+}
